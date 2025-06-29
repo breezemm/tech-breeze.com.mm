bf8f69b10e4701b0dbbfbeb1758b8510bfa54186
ayeseintthu <ayesaintthumm2023@gmail.com>
2025-06-29 17:15:28 +0630

    feat: close responsive-home-section
    Signed-off-by: ayeseintthu <ayesaintthumm2023@gmail.com>

--- app/sections/home-section.tsx
+++ app/sections/home-section.tsx
@@ -1,7 +1,7 @@
 export default function HomeSection() {
   return (
     <section className="bg-seasalt lg:130 flex h-70.5 flex-col items-center justify-center transition-all sm:h-100 xl:h-165" id="home">
-      <div className="text-eerie-black flex items-center space-x-2.5 text-nowrap transition-all sm:space-x-5 md:space-x-6 md:text-2xl lg:space-x-8 lg:text-3xl xl:text-5xl">
+      <div className=" text-eerie-black flex items-center space-x-2.5 text-nowrap transition-all sm:space-x-5 md:space-x-6 md:text-2xl lg:space-x-8 lg:text-[2.813rem] xl:text-5xl">
         <div>
           <h1>Crafting technology feel</h1>
         </div>
