3e1383b7a16b779dc217e0afeca9ec1548bc817a
La Pyae Hmue Aug <lapyaehmueaug@192.168.5.103>
2025-06-29 07:32:05 +0700

    fix: add id's on sections

--- components/app-footer.tsx
+++ components/app-footer.tsx
@@ -1,45 +1,53 @@
 const developers = [
-  { name: 'Ko Pyae Phyo Hlaing(Full-stack Developer)', socialLink: '' },
   { name: 'Ma Aye Saint Thu(Full-stack Developer)', socialLink: '' },
-  { name: 'Ma Paing Hus Wai Kyaw(Product Manager)', socialLink: '' },
   { name: 'Ko Khant Nyar Thwin (Full-stack Developer)', socialLink: '' },
-  { name: 'Ko La Pae Hume Aung (Full-stack Developer)', socialLink: '' },
+  { name: 'Ko La Pyae Hume Aung (Frontend Developer)', socialLink: 'https://www.linkedin.com/in/lapyaehmueaung/' },
+  { name: 'Ma Paing Hus Wai Kyaw(Product Manager)', socialLink: '' },
+  { name: 'Ko Pyae Phyo Hlaing(Full-stack Developer)', socialLink: '' },
   { name: 'Ko Zawe Yan Naing (Full-stack Developer)', socialLink: '' },
   { name: 'Ko Zwe Mhan Shin (Product Designer)', socialLink: '' },
 ]
 
 const breezeInfo = [
-  { label: 'Mail', href: 'hello@tech-breeze.com.mm' },
-  { label: 'Website', href: 'www.tech-breeze.com.mm' },
-  { label: 'Phone number', href: '09 980 40 96 20' },
+  { label: 'Mail', href: 'mailto:hello@tech-breeze.com.mm', text: 'hello@tech-breeze.com.mm' },
+  { label: 'Website', href: 'https://www.tech-breeze.com.mm', text: 'www.tech-breeze.com.mm' },
+  { label: 'Phone number', href: 'tel:+959980409620', text: '09 980 40 96 20' },
 ]
 
 export default function AppFooter() {
   return (
-    <footer className="bg-seasalt flex gap-42 py-12 pl-5 text-[10px] md:pl-6 md:text-xl">
-      <div className="flex w-full flex-col items-center justify-center gap-6 lg:w-1/2">
-        <h3 className="ml-[-7rem] font-semibold md:ml-[-15rem]">Address</h3>
-        <div className="flex flex-col">
-          <p>Tech Breeze</p>
-          <p>Based in Myanmar (Remote-First)</p>
-          {breezeInfo.map(({ label, href }) => (
-            <p key={label}>
-              <span>{label}: </span>
-              <a href={href}>{href}</a>
-            </p>
-          ))}
+    <footer className="bg-seasalt mt-10 py-12">
+      <div className="flex justify-center lg:space-x-[118px]">
+        {/* address */}
+        <div>
+          <h3 className="mb-6 font-semibold">Address</h3>
+          <div>
+            <p>Tech Breeze</p>
+            <p>Based in Myanmar (Remote-First)</p>
+            {breezeInfo.map(({ label, href, text }) => (
+              <p key={label}>
+                <span>{label}: </span>
+                <a target="_blank" rel="noopener noreferrer" href={href}>
+                  {text}
+                </a>
+              </p>
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="hidden w-3/4 flex-col gap-6 lg:flex">
-        <h3>A big thank to our awesome Breezers for crafting website to life!</h3>
-        <div className="flex flex-col">
-          {developers.map(({ name, socialLink }) => (
-            <a className="underline-off-set-8 leading-9 underline" href={socialLink} key={name}>
-              {name}
-              <br />
-            </a>
-          ))}
+        {/* developers */}
+        <div>
+          <div className="hidden flex-col gap-6 lg:flex">
+            <h3>A big thank to our awesome Breezers for crafting website to life!</h3>
+            <div className="flex flex-col">
+              {developers.map(({ name, socialLink }) => (
+                <a target="_blank" rel="noopener noreferrer" className="underline-off-set-8 leading-9 underline" href={socialLink} key={name}>
+                  {name}
+                  <br />
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </footer>
--- components/app-header.tsx
+++ components/app-header.tsx
@@ -22,11 +22,11 @@ export default function AppHeader() {
   return (
     <>
       <header className="fixed flex w-full items-center justify-between bg-white px-[22px] py-[11px] md:px-[15px] md:py-[30px] lg:px-24">
-        {!isNavOpen && <h2 className="text-[25px]">Tech Breeze</h2>}
+        <h2 className="text-[25px]">Tech Breeze</h2>
         <AppNavbar className="hidden md:flex" navItems={navItems} />
         <div className="hidden items-center space-x-8 md:flex">
           {links.map((link) => (
-            <a key={link.href} href={link.href}>
+            <a target="_blank" rel="noopener noreferrer" key={link.href} href={link.href}>
               <link.icon />
             </a>
           ))}
@@ -34,13 +34,16 @@ export default function AppHeader() {
         {!isNavOpen && <Menu onClick={() => setIsNavOpen(true)} className="block md:hidden" />}
         {isNavOpen && (
           <>
-            <div onClick={() => setIsNavOpen(false)} className="bg-eerie-black/40 fixed top-0 right-0 bottom-0 left-0 h-screen w-[100vh] md:hidden" />
-            <div onClick={() => setIsNavOpen(false)} className="fixed top-0 left-0 z-20 flex w-full flex-col bg-white md:hidden">
+            <div
+              onClick={() => setIsNavOpen(false)}
+              className="bg-eerie-black/40 fixed top-0 right-0 bottom-0 left-0 z-50 h-screen w-[100vh] md:hidden"
+            />
+            <div onClick={() => setIsNavOpen(false)} className="fixed top-0 left-0 z-50 flex w-full flex-col bg-white md:hidden">
               <X onClick={() => setIsNavOpen(false)} className="absolute top-0 right-0 mx-4 my-4" />
               <AppNavbar className="flex flex-col gap-[10px] px-8 py-[4rem] md:hidden" navItems={navItems} />
               <div className="mb-10 flex items-center gap-4 px-8">
                 {links.map((link) => (
-                  <a className="underline underline-offset-4" key={link.href} href={link.href}>
+                  <a target="_blank" rel="noopener noreferrer" className="underline underline-offset-4" key={link.href} href={link.href}>
                     {link.label}
                   </a>
                 ))}
