602d3e63b6630d71f382c9112a44ca04248a7e8f
CodekageNyRR <justicforyang@gmail.com>
2025-07-01 11:01:32 +0630

    fix: responsive-about-section
    Signed-off-by: CodekageNyRR <justicforyang@gmail.com>

--- components/cards/about-card.tsx
+++ components/cards/about-card.tsx
@@ -5,9 +5,9 @@ interface AboutCardProps {
 
 export default function AboutCard({ title, description }: AboutCardProps) {
   return (
-    <div className="bg-seasalt flex flex-col items-center justify-center gap-y-2.5 px-8.5 py-12.5 sm:gap-8 md:px-10">
-      <h1 className="text-xss text-center sm:text-sm md:text-base">{title}</h1>
-      <p className="text-sm md:text-xl lg:text-3xl">{description}</p>
+    <div className="bg-seasalt flex flex-col items-center justify-center gap-y-2.5 px-8.5 py-12.5 sm:gap-8 md:px-10 lg:px-12 xl:px-14 2xl:px-14 transition-all   ">
+      <p className="text-sm text-center sm:text-sm md:text-base lg:text-lg  transition-all">{title}</p>
+      <p className="text-sm md:text-xl lg:text-3xl text-justify transition-all">{description}</p>
     </div>
   )
 }
