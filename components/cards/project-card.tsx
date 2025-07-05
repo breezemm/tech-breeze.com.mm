027547b06a82e7d45fc3be92fb805585edbf333e
Zawe Yan Naing <157109900+ZaweYanNaing@users.noreply.github.com>
2025-07-05 11:12:33 +0630

    fix : text-align and photo size (#30)
    Signed-off-by: Zawe Yan Naing <zaweyannaing121.phys@gmail.com>

--- components/cards/project-card.tsx
+++ components/cards/project-card.tsx
@@ -12,22 +12,22 @@ export default function ProjectCard({ title, description, leftImage, rightImage 
     <div className="bg-seasalt flex w-full flex-col px-11.25 py-3.75 md:px-10 md:py-12.5 lg:px-28 lg:py-8">
       <h2 className="text-xss text-center font-normal md:text-base lg:text-xl">Our projects</h2>
       <h1 className="mt-2.5 text-sm font-medium md:mt-4 md:text-xl lg:mt-8 lg:text-3xl">{title}</h1>
-      <p className="mt-1.25 text-justify text-sm font-normal md:mt-2 md:text-xl lg:mt-6 lg:text-3xl">{description}</p>
+      <p className="mt-1.25 text-sm font-normal md:mt-2 md:text-xl lg:mt-6 lg:max-w-264.25 lg:text-3xl max-w-80 md:max-w-250">{description}</p>
 
-      <div className="flex gap-x-1.5 md:gap-x-3">
+      <div className="flex gap-x-1.5 md:gap-x-3 lg:gap-x-6">
         <Image
           src={leftImage}
           width="4096"
           height="4096"
           alt="left-cover"
-          className="mt-1.25 w-1/2 rounded-xs object-cover object-center md:mt-6 md:rounded-md"
+          className="mt-1.25 w-1/2 rounded-xs object-cover object-center md:mt-6 md:rounded-md lg:max-h-62.5 lg:max-w-121 max-w-38 max-h-20 md:max-w-115 md:max-h-50"
         />
         <Image
           src={rightImage}
           width="4096"
           height="4096"
           alt="right-cover"
-          className="mt-1.25 w-1/2 rounded-xs object-cover object-center md:mt-6 md:rounded-md"
+          className="mt-1.25 w-1/2 rounded-xs object-cover object-center md:mt-6 md:rounded-md lg:max-h-62.5 lg:max-w-121 max-w-38 max-h-20 md:max-w-115 md:max-h-50"
         />
       </div>
     </div>
